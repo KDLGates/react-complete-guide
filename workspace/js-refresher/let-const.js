55ed8685e3a8fcd55354aaa0b050105835a14584
Kristopher Gates <kdlgates@kdlgates.com>
2022-12-01 16:30:53 -0500

    init

---
+++ workspace/js-refresher/let-const.js
@@ -0,0 +1,9 @@
+let myName = "Kris";
+console.log("Mihi nomen est " + myName + "us.");
+
+const constName = "Kris";
+
+/* 
+Throws TypeError: Assignment to constant variable.
+// constName = "Krisus";
+ */
