ff9dcb28c8c038bfaa39a95cc092c7b15e87a625
Kristopher Gates <kdlgates@kdlgates.com>
2022-12-03 18:16:37 -0500

    finished Practice 1

--- projects/expense-tracker/src/components/ExpenseItem.js
+++ projects/expense-tracker/src/components/ExpenseItem.js
@@ -4,14 +4,12 @@ import "./ExpenseItem.css";
 
 // takes title, amount, date
 export default function ExpenseItem(props) {
-  const title = props.title;
-  const amount = props.amount;
   return (
     <div className="expense-item">
       <ExpenseItemDate date = {props.date} />
       <div className="expense-item__description">
-        <h2>{title}</h2>
-        <div className="expense-item__price">${amount}</div>
+        <h2>{props.title}</h2>
+        <div className="expense-item__price">${props.amount}</div>
       </div>
     </div>
   );
