33e99c6cb93966a985b5da9b4fe55a8faca8adf3
Kristopher Gates <kdlgates@kdlgates.com>
2022-12-05 18:03:10 -0500

    Conclude Section IV & stateless ExpenseItem

--- projects/expense-tracker/src/components/Expenses/ExpenseItem.js
+++ projects/expense-tracker/src/components/Expenses/ExpenseItem.js
@@ -1,27 +1,17 @@
-import React, { useState } from "react";
+import React from "react";
 import Card from "../UI/Card";
 import ExpenseItemDate from "./ExpenseItemDate";
 import "./ExpenseItem.css";
 
 // takes title, amount, date
 export default function ExpenseItem(props) {
-  // Array destructuring of useState array
-  // [value, React.Dispatch] is best assigned via array destructuring
-  const [title, setTitle] = useState(props.title);
-
-  const clickHandler = () => {
-    console.log("Updating " + title);
-    setTitle(title + " (Clicked)");
-  }
-
   return (
     <Card className="expense-item">
-      <ExpenseItemDate date = {props.date} />
+      <ExpenseItemDate date={props.date} />
       <div className="expense-item__description">
-        <h2>{title}</h2>
+        <h2>{props.title}</h2>
         <div className="expense-item__price">${props.amount}</div>
       </div>
-      <button onClick={clickHandler}>Click Me</button>
     </Card>
   );
 }
